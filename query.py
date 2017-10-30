1803d994901ddff7690e1408a2c9e84c5b73cf07
Ryan Siu <ryansiu@uw.edu>
2017-10-30 16:25:20 +0000

    Complete the first two queries

--- query.py
+++ query.py
@@ -1,7 +1,7 @@
 import psycopg2 as psql
 
 # Simple remark in table structure:
-# 
+#
 # TABLE articles (
 #     author integer NOT NULL,
 #     title text NOT NULL,
@@ -11,13 +11,13 @@ import psycopg2 as psql
 #     "time" timestamp with time zone DEFAULT now(),
 #     id integer NOT NULL
 # )
-# 
+#
 # TABLE authors (
 #     name text NOT NULL,
 #     bio text,
 #     id integer NOT NULL
 # )
-# 
+#
 # TABLE log (
 #     path text,
 #     ip inet,
@@ -27,9 +27,64 @@ import psycopg2 as psql
 #     id integer NOT NULL
 # )
 
+
 class Logs:
-	def __init__(self, name):
-		self.name = name
+    def __init__(self, database):
+        self.database = database
+        self.connect = psql.connect("dbname=" + self.database)
+        # Fetch the number of authors
+        cursor = self.connect.cursor()
+        cursor.execute(
+            '''
+            select count(*) from authors
+            ''')
+        self.authorNumber = cursor.fetchone()[0]
+        # Fetch the number of articles
+        cursor = self.connect.cursor()
+        cursor.execute(
+            '''
+            select count(*) from articles
+            ''')
+        self.articleNumber = cursor.fetchone()[0]
+
+    def popularArticles(self, number=None):
+        # Set the default value of number of articles from self
+        if not number:
+            number = self.articleNumber
+        cursor = self.connect.cursor()
+        cursor.execute(
+            '''
+            select articles.title, count(log.path) as counting
+            from articles join log on log.path = (%s) || articles.slug
+            group by articles.title
+            order by counting desc
+            limit (%s)
+            ''', ("/article/", number))
+        return cursor.fetchall()
+
+    def popularAuthors(self, number=None):
+        # Set the default value of number of authors from self
+        if not number:
+            number = self.authorNumber
+        cursor = self.connect.cursor()
+        cursor.execute(
+            '''
+            select quicklook.name, count(log.path) as counting
+            from (select authors.name, (%s) || articles.slug as url
+            from authors right join articles
+            on authors.id = articles.author) as quickLook left join log
+            on log.path = quicklook.url
+            group by quicklook.name
+            order by counting desc
+            limit (%s)
+            ''', ("/article/", number))
+        return cursor.fetchall()
+
+    def errorReport(self, percentage):
+        cursor = self.connect.cursor()
+        cursor.execute()
+        return cursor.fetchall()
 
 if __name__ == "__main__":
-	log = Logs('h')
+    log = Logs('news')
+    print(log.popularAuthors())
